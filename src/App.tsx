e96f2db81671803a208b019590546ce614b884f8
Mathilde <mathildewaerstad@gmail.com>
2021-01-26 08:01:20 +0100

    gruppere på kategori og liste us

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,122 @@
-import React, { ChangeEvent, useState } from "react";
-import "./App.css";
-import * as XLSX from "xlsx";
-import { addDays } from "date-fns";
+import { addDays } from 'date-fns';
+import groupBy from 'lodash.groupby';
+import React, { ChangeEvent, useState } from 'react';
+import * as XLSX from 'xlsx';
+import './App.css';
+import transactions from './transactions';
 
-interface Transaction {
-  amount: number;
-  bookDate: Date;
-  currency: string;
-  area: string;
-  category: string;
-  description: string;
-  transactionDate: Date;
-  type: string;
-}
+// interface Transaction {
+//     amount: number;
+//     bookDate: Date;
+//     currency: string;
+//     area: string;
+//     category: string;
+//     description: string;
+//     transactionDate: Date;
+//     type: string;
+// }
 
 const App: React.FC = () => {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
+    // const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  const onUpload = async (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+    // const onUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    //     event.preventDefault();
+    //
+    //     if (event.target.files) {
+    //         const newDocumentsToUpload = Array.from(event.target.files);
+    //
+    //         const f = newDocumentsToUpload[0];
+    //         const reader = new FileReader();
+    //         reader.onload = (e) => {
+    //             if (e && e.target && e.target.result) {
+    //                 const data = new Uint8Array(e.target.result as ArrayBufferLike);
+    //                 const workbook = XLSX.read(data, { type: 'array' });
+    //
+    //                 const sheetNameList = workbook.SheetNames;
+    //
+    //                 const referenceDate = new Date('1899-12-30');
+    //                 const allTranscations = XLSX.utils
+    //                     .sheet_to_json(workbook.Sheets[sheetNameList[0]])
+    //                     .map((transaction: any) => ({
+    //                         amount: transaction.Amount,
+    //                         bookDate: addDays(referenceDate, transaction.BookDate),
+    //                         currency: transaction.Currency,
+    //                         area: transaction['Merchant Area'],
+    //                         category: transaction['Merchant Category'],
+    //                         description: transaction.Text,
+    //                         transactionDate: addDays(referenceDate, transaction.TransactionDate),
+    //                         type: transaction.Type,
+    //                     }));
+    //
+    //                 const fileData = JSON.stringify(allTranscations);
+    //                 const blob = new Blob([fileData], { type: 'text/plain' });
+    //                 const url = URL.createObjectURL(blob);
+    //                 const link = document.createElement('a');
+    //                 link.download = 'filename.json';
+    //                 link.href = url;
+    //                 link.click();
+    //
+    //                 setTransactions(allTranscations);
+    //             }
+    //         };
+    //
+    //         reader.readAsArrayBuffer(f);
+    //     }
+    // };
 
-    if (event.target.files) {
-      const newDocumentsToUpload = Array.from(event.target.files);
+    const transactionsGroupedByCategory = groupBy(transactions, 'category');
+    const transactionsWithTotalAmountForCategory: Record<
+        string,
+        { totalAmount: number; count: number }
+    > = Object.keys(transactionsGroupedByCategory).reduce((allTransactions, key) => {
+        const transactionsForCategory = transactionsGroupedByCategory[key];
+        const totalAmountForCategory = transactionsForCategory.reduce(
+            (amount, transaction) => amount + transaction.amount,
+            0
+        );
 
-      const f = newDocumentsToUpload[0];
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        if (e && e.target && e.target.result) {
-          var data = new Uint8Array(e.target.result as ArrayBufferLike);
-          var workbook = XLSX.read(data, { type: "array" });
+        return {
+            ...allTransactions,
+            [key]: { totalAmount: totalAmountForCategory, count: transactionsForCategory.length },
+        };
+    }, {});
 
-          var sheet_name_list = workbook.SheetNames;
-
-          const referenceDate = new Date("1899-12-30");
-          const allTranscations = XLSX.utils
-            .sheet_to_json(workbook.Sheets[sheet_name_list[0]])
-            .map((transaction: any) => ({
-              amount: transaction["Amount"],
-              bookDate: addDays(referenceDate, transaction["BookDate"]),
-              currency: transaction["Currency"],
-              area: transaction["Merchant Area"],
-              category: transaction["Merchant Category"],
-              description: transaction["Text"],
-              transactionDate: addDays(
-                referenceDate,
-                transaction["TransactionDate"]
-              ),
-              type: transaction["Type"],
-            }));
-
-          setTransactions(allTranscations);
+    const totalAmount = Object.keys(transactionsWithTotalAmountForCategory).reduce((total, transaction) => {
+        const amount = transactionsWithTotalAmountForCategory[transaction].totalAmount;
+        if (amount < 0) {
+            return total + transactionsWithTotalAmountForCategory[transaction].totalAmount;
         }
-      };
 
-      reader.readAsArrayBuffer(f);
-    }
-  };
+        return total;
+    }, 0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Blir du oppdatert fortsatt?</p>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Blir du oppdatert fortsatt?</p>
 
-        <div>
-          <label>
-            <input type="file" onChange={onUpload} accept="file" />
-          </label>
+                <h1>{totalAmount}</h1>
+
+                <ul>
+                    {Object.keys(transactionsWithTotalAmountForCategory).map((transaction, index) => (
+                        <li key={index}>
+                            <span>
+                                {transaction}[{transactionsWithTotalAmountForCategory[transaction].count}]
+                            </span>
+                            :<span>{transactionsWithTotalAmountForCategory[transaction].totalAmount}</span>
+                        </li>
+                    ))}
+                </ul>
+                <ul>
+                    {transactions.map((transaction, index) => (
+                        <li key={index}>
+                            <span>{transaction.description}</span>:<span>{transaction.amount}</span>
+                        </li>
+                    ))}
+                </ul>
+            </header>
         </div>
-
-        <ul>
-          {transactions.map((transaction, index) => (
-            <li key={index}>
-              <span>{transaction.description}</span>:
-              <span>{transaction.amount}</span>
-            </li>
-          ))}
-        </ul>
-      </header>
-    </div>
-  );
+    );
 };
 
 export default App;
